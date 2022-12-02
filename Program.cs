c1bfa5964bc3d69b29676ce3420eab17a3eafc18
Julian <maksimovyou@gmail.com>
2022-12-03 05:02:47 +0900

    Написал InputArray

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(string[] array)
+{
+
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"Введите {j + 1} строку >>>");
+        array[j] = Console.ReadLine();
+    }
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+
+}
+
+
+
+Console.Write("Введите количество кустов >>> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[n];
+
+
+
+
+InputArray(array);
