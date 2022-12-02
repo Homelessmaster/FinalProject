a61898e7b237804d6cffb339980ff6825f5fa9a1
Julian <maksimovyou@gmail.com>
2022-12-03 05:20:53 +0900

    Написал ChangeArray

--- Program.cs
+++ Program.cs
@@ -10,9 +10,33 @@
 
 }
 
+void ChangeArray(string[] array, int n)
+{
+    int a = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[j].Length > 3)
+            a++;
+    }
+    string[] result = new string[n - a];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int b = 0;
+        if(array[i].Length <= 3)
+            result[b] = array[i];
+            b++;
+
+    }
 
 
-Console.Write("Введите количество кустов >>> ");
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+
+}
+
+
+
+Console.Write("Введите количество строк >>> ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string[] array = new string[n];
@@ -20,4 +44,6 @@ string[] array = new string[n];
 
 
 
+
 InputArray(array);
+ChangeArray(array, n);
