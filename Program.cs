0bdd72342f89b19518b46c859f8da2cea2a29b1c
Julian <maksimovyou@gmail.com>
2022-12-03 16:27:49 +0900

    Поправил вид кода

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿void InputArray(string[] array)
 {
-
     for (int j = 0; j < array.Length; j++)
     {
         Console.Write($"Введите {j + 1} строку >>>");
-        array[j] = Console.ReadLine();
+        array[j] = Convert.ToString(Console.ReadLine());
     }
-    Console.WriteLine($"[{string.Join(", ", array)}]");
 
+    Console.WriteLine($"[{string.Join(", ", array)}]");
 }
 
 
@@ -16,32 +15,32 @@ void ChangeArray(string[] array, int n)
 {
     int a = 0;
     int b = 0;
+    string[] result = new string[n - a];
+
 
     for (int j = 0; j < array.Length; j++)
     {
         if(array[j].Length > 3)
             a++;
     }
-    string[] result = new string[n - a];
+
 
     for (int i = 0; i < array.Length; i++)
-    {
-        
+    {   
         if(array[i].Length < 4)
         {
             result[b] = array[i];
             b++;
         }
-
     }
     Console.WriteLine($"[{string.Join(" ", result)}]");
 }
 
 
 Console.Clear();
+
 Console.Write("Введите количество строк >>> ");
 int n = Convert.ToInt32(Console.ReadLine());
-
 string[] array = new string[n];
 
 InputArray(array);
