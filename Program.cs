998f12cd7bf4ad43c7b01029c063ea62d62e455a
Julian <maksimovyou@gmail.com>
2022-12-03 16:24:43 +0900

    Продебажил

--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@
 
 }
 
+
+
 void ChangeArray(string[] array, int n)
 {
     int a = 0;
+    int b = 0;
+
     for (int j = 0; j < array.Length; j++)
     {
         if(array[j].Length > 3)
@@ -22,28 +26,23 @@ void ChangeArray(string[] array, int n)
 
     for (int i = 0; i < array.Length; i++)
     {
-        int b = 0;
-        if(array[i].Length <= 3)
+        
+        if(array[i].Length < 4)
+        {
             result[b] = array[i];
             b++;
+        }
 
     }
-
-
-    Console.WriteLine($"[{string.Join(", ", array)}]");
-
+    Console.WriteLine($"[{string.Join(" ", result)}]");
 }
 
 
-
+Console.Clear();
 Console.Write("Введите количество строк >>> ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string[] array = new string[n];
-
-
-
-
 
 InputArray(array);
 ChangeArray(array, n);
